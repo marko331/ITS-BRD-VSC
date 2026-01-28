11dc7a42c60212ad21293a6c37db636f9849829e
FranzKorf <frakorf@gmail.com>
2026-01-28 21:14:28 +0100

    Temporäre Dateien aus git entfernt.

--- Programs/Schiebepuzzle/tmp/roots.cmake
+++
@@ -1,7 +0,0 @@
-# roots.cmake
-set(CMSIS_PACK_ROOT "/Users/franz/.cache/arm/packs" CACHE PATH "CMSIS pack root")
-cmake_path(ABSOLUTE_PATH CMSIS_PACK_ROOT NORMALIZE OUTPUT_VARIABLE CMSIS_PACK_ROOT)
-set(CMSIS_COMPILER_ROOT "/Users/franz/.vscode/extensions/arm.cmsis-csolution-1.64.2-darwin-arm64/tools/cmsis-toolbox/etc" CACHE PATH "CMSIS compiler root")
-cmake_path(ABSOLUTE_PATH CMSIS_COMPILER_ROOT NORMALIZE OUTPUT_VARIABLE CMSIS_COMPILER_ROOT)
-set(SOLUTION_ROOT "/Users/franz/GIT_HUB/ML_GSP_VSC_KRF/Programs/Schiebepuzzle" CACHE PATH "CMSIS solution root")
-cmake_path(ABSOLUTE_PATH SOLUTION_ROOT NORMALIZE OUTPUT_VARIABLE SOLUTION_ROOT)
