99ca7356574772d640e03e6938896a25bf1c3a21
Marko Petreski <markopetreski@Markos-MacBook-Pro.local>
2026-06-11 11:32:37 +0200

    Woche 1 Stoppuhr

---
+++ Programs/StoppuhrASM/Src/README.md
@@ -0,0 +1,26 @@
+Stoppuhr - Woche 1
+
+## Ziel der Aufgabe 
+Es soll eine Stoppuhr in Assembler entwickelt werden.
+Die Bedienung erfolgt später über die Taster S5, S6 und S7.
+Die Zustände der Stoppuhr sind INIT, RUN und HOLD
+Die LEDs D8 und D9 zeigen später den Zustand der Stoppuhr an.
+
+## Anforderungen 
+- Anzeige der Zeit auf dem TFT-Display im Format mm:ss.nn
+- S7 startet die Stoppuhr bzw. setzt sie aus HOLD fort
+- S6 hält die Anzeige an
+- S5 setzt die Stoppuhr zurück
+- D8 leuchtet bei aktiver Zeitmessung
+- D9 leuchtet im Zustand HOLD
+
+## Teilaufgaben
+
+### Woche 1
+
+- Display ansteuern und testen
+- Taster einlesen und testen
+- LEDs ansteuern und testen
+- Beispielcode in Unterprogramme aufteilen
+
+### Woche 2
---
+++ Programs/StoppuhrASM/Src/main.s
@@ -0,0 +1,110 @@
+;******************** (C) COPYRIGHT HAW-Hamburg ********************************
+;* File Name          : main.s
+;* Author             : Franz Korf	
+;* Version            : V1.0
+;* Date               : 11.05.2022
+;* Description        : Rahmen zur Loesung von GTP Woche 7-9 (Stoppuhr).
+;
+;*******************************************************************************
+
+; Define address of selected GPIO and Timer registers
+PERIPH_BASE     	equ	0x40000000                 ;Peripheral base address
+AHB1PERIPH_BASE 	equ	(PERIPH_BASE + 0x00020000)
+APB1PERIPH_BASE     equ PERIPH_BASE
+
+GPIOD_BASE			equ	(AHB1PERIPH_BASE + 0x0C00)
+GPIOF_BASE			equ	(AHB1PERIPH_BASE + 0x1400)
+TIM2_BASE           equ (APB1PERIPH_BASE + 0x0000)
+	
+GPIO_F_PIN        	equ	(GPIOF_BASE + 0x10)
+
+GPIO_D_PIN			equ	(GPIOD_BASE + 0x10)
+GPIO_D_SET			equ (GPIOD_BASE + 0x18)
+GPIO_D_CLR			equ	(GPIOD_BASE + 0x1A)
+	
+TIMER				equ (TIM2_BASE + 0x24)   ; CNT : current time stamp (32 bit),  resolution
+TIM2_PSC			equ (TIM2_BASE + 0x28)   ; Prescaler  resolution
+TIM2_ERG			equ (TIM2_BASE + 0x14)   ; 16 Bit register, Bit 0 : 1 Restart Timer
+
+
+    EXTERN initITSboard
+    EXTERN GUI_init
+	EXTERN TP_Init
+	EXTERN initTimer
+	EXTERN lcdSetFont
+	EXTERN lcdGotoXY      		; TFT goto x y function
+	EXTERN lcdPrintS			; TFT output function	
+    EXTERN lcdPrintC            ; TFT output one character		
+	EXTERN Delay				; Delay (ms) function
+
+
+;********************************************
+; Data section, aligned on 4-byte boundery
+;********************************************
+	AREA MyData, DATA, align = 2
+
+DEFAULT_BRIGHTNESS	DCW     800
+MY_TEXT				DCB		"00:00.00",0
+
+;********************************************
+; Code section, aligned on 8-byte boundery
+;********************************************
+	AREA |.text|, CODE, READONLY, ALIGN = 3
+
+
+;--------------------------------------------
+; main subroutine
+;--------------------------------------------
+	EXPORT main [CODE]
+	
+main	PROC
+
+		; Initialisierung der HW
+		BL		initITSboard
+		ldr   	r1, =DEFAULT_BRIGHTNESS
+		ldrh 	r0, [r1]
+		bl   	GUI_init
+		bl  	initTimer
+		ldr 	R1,=TIM2_PSC   			; Set pre scaler such that 1 timer tick represents 10 us
+		mov 	R0,#(90*10-1) 
+		strh	R0,[R1]
+		ldr 	R1,=TIM2_ERG   			; Restart timer	
+		mov		R0,#0x01
+		strh	R0,[R1]					; Set UG Bit
+		MOV 	R0, #24
+		bl  	lcdSetFont
+
+		; Ihre Initialisierung
+
+		; Simple test code
+		LDR  R0,=MY_TEXT
+		BL  	lcdPrintS
+
+superloop
+		 BL readButtons
+		 BL setLEDs
+   		 BAL superloop
+		ENDP
+
+
+readButtons PROC
+    LDR  R0,=GPIO_F_PIN
+    LDRH R0,[R0]
+    AND  R0,#0xFF
+    BX   LR
+    ENDP
+
+setLEDs PROC
+    LDR R1,=GPIO_D_CLR
+    STR R0,[R1]
+
+    EOR R0,R0,#0xFF
+
+    LDR R1,=GPIO_D_SET
+    STR R0,[R1]
+
+    BX LR
+    ENDP
+	
+		ALIGN
+		END
